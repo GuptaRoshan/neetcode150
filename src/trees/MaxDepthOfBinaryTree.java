42e1c5fef358cadc85e4c601e74fb8700924a4ef
Roshan Gupta <roshankgupta13@gmail.com>
2024-04-17 16:31:38 +0530

    trees and dp

---
+++ src/trees/MaxDepthOfBinaryTree.java
@@ -0,0 +1,45 @@
+package trees;
+
+public class MaxDepthOfBinaryTree {
+
+    //Definition for a binary tree node.
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    /**
+     * To find the depth of a node
+     *
+     * @param root root node
+     * @return depth of the node
+     */
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        System.out.println(maxDepth(root));
+    }
+}
